097b58034b6dc059e2c5d45c69636111f5acbaf8
Martorelli <martorelli_biel@outlook.com>
2022-11-05 23:47:58 -0300

    part 2 add controls for music

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const controls = document.querySelector("#controls");
+const btnPlay = document.querySelector("#play-control");
 
 let index = 0;
 let currentMusic;
@@ -7,6 +8,7 @@ let isPlaying = false;
 controls.addEventListener("click", function (event) {
   const audios = [];
   let music = {};
+
   if (event.target.id != "controls") {
     const musics =
       event.path[2].childNodes[3].childNodes[5].childNodes[1].childNodes[3]
@@ -14,14 +16,53 @@ controls.addEventListener("click", function (event) {
 
     musics.forEach(function (item) {
       if (item.nodeName != "#text") {
-        music.nome = item.childNodes[3].childNodes[0].data;
-        music.artista = item.childNodes[5].childNodes[0].data;
-        music.imagem = item.childNodes[1].childNodes[0].currentSrc;
+        music.name = item.childNodes[3].childNodes[0].data;
+        music.artist = item.childNodes[5].childNodes[0].data;
+        music.image = item.childNodes[1].childNodes[0].currentSrc;
         music.audio = item.childNodes[7].childNodes[1];
-
         audios.push(music);
         music = {};
       }
     });
   }
+
+  function updateDataMusic() {
+    currentMusic = audios[index];
+    document.querySelector("#currentImg").src = currentMusic.image;
+    document.querySelector("#currentName").innerText = currentMusic.name;
+    document.querySelector("#currentArtist").innerText = currentMusic.artist;
+    document.querySelector("#volume").value = currentMusic.audio.volume * 100;
+
+    const progressbar = document.querySelector("#progressbar");
+    const textCurrentDuration = document.querySelector("#current-duration");
+    const textTotalDuration = document.querySelector("#total-duration");
+
+    progressbar.max = currentMusic.audio.duration;
+    textTotalDuration.innerText = secondsToMinuts(currentMusic.audio.duration);
+    textCurrentDuration.innerText = secondsToMinuts(
+      currentMusic.audio.currentTime
+    );
+    progressbar.valueAsNumber = currentMusic.audio.currentTime;
+  }
+
+  if (event.target.id == "play-control") {
+    if (index === 0) {
+      updateDataMusic();
+    }
+    if (!isPlaying) {
+      btnPlay.classList.replace("bi-play-fill", "bi-pause-fill");
+      currentMusic.audio.play();
+      isPlaying = true;
+    } else {
+      btnPlay.classList.replace("bi-pause-fill", "bi-play-fill");
+      currentMusic.audio.pause();
+      isPlaying = false;
+    }
+  }
 });
+
+function secondsToMinuts(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`;
+}
